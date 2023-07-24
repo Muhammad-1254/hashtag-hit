e7ae28536afd93af41cc68e224ccfd7227f97f22
Muhammad usman <107724827+Muhammad-1254@users.noreply.github.com>
2023-07-24 13:53:11 +0500

    Add files via upload

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PageLoading } from './components'
+
+const loading = () => {
+  return (
+    <PageLoading styles={""} iconSize={350}/>
+  )
+}
+
+export default loading
---
+++ src/app/page.tsx
@@ -0,0 +1,65 @@
+
+import {
+  LifestyleBeautyIInspiration,
+  Fictional,
+  FeaturedPost,
+  History,
+  Psychology,
+  ParentingGuide,
+  FoodBlogAndTravel,
+  Science,
+  
+ 
+} from '@/app/components';
+
+import {  getCategoriesPost } from '@/app/services';
+
+export const revalidate = 3600
+export default async function Home() {
+
+  const data:any = await getCategoriesPost()
+
+
+  const {
+    travelAPI,
+    featuredPostAPI,
+    psychologyAPI,
+    historyAPI,
+    parentingGuideAPI,
+    lifestyleAPI,
+    beautyAndTipsAPI,
+    fictionalAPI,
+    scienceAPI,
+    foodBlogAPI,
+    InspirationalAPI,
+  } = data;
+// console.log("data: ",data);
+
+   
+  return (
+    
+    <main>
+   
+      
+      {/* <Suspense fallback={<PageLoading styles={""} iconSize={150}/>}> */}
+        
+      <div className="max-w-[370px]  md:max-w-[750px] lg:max-w-[1800px] mx-auto">
+        <FeaturedPost featuredPostAPI={featuredPostAPI}/>
+         <Psychology psychologyAPI={psychologyAPI} />
+        <History historyAPI={historyAPI} />
+        <Science scienceAPI={scienceAPI} /> 
+
+      </div>
+       <ParentingGuide parentingGuideAPI={parentingGuideAPI} />
+      <div className="md:max-w-5xl lg:max-w-[1920px] mx-auto">
+         <LifestyleBeautyIInspiration
+         props={{ lifestyleAPI, InspirationalAPI, beautyAndTipsAPI }}/>  
+        <Fictional fictionalAPI={fictionalAPI} />
+      </div>
+      <FoodBlogAndTravel props={{ travelAPI, foodBlogAPI }} />
+    
+          {/* </Suspense> */}
+          
+      </main>
+  );
+}
